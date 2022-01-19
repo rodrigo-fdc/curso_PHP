6d0b4219098a97c21eeabe97598cedd49c3f5e4f
Rodrigo Ferreira <rodrigoferreira.mkt@gmail.com>
2022-01-19 18:04:33 -0300

    Mudança de pasta

---
+++ doadorSangue.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doador de Sangue</title>
+</head>
+<body>
+    <?php    
+        $idade = 25;
+        $peso =  82.3;
+
+        if ( $idade >= 16 && idade <= 69 && $peso >= 50) {
+            echo 'Atende aos requisitos';
+        };
+
+    ?>
+</body>
+</html>
